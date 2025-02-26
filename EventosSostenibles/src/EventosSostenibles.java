3c99da654d1891bf9b8f48bb57a90b74561edad8
usuario <usuario@DESKTOP-5H8OPMC.home>
2025-02-27 00:56:44 +0100

    Segundo commit (dos primeros metodos)

--- EventosSostenibles/src/EventosSostenibles.java
+++ EventosSostenibles/src/EventosSostenibles.java
@@ -23,10 +23,10 @@ public class EventosSostenibles {
 			opcion=sc.nextInt();
 			switch(opcion) {
 			case 1:
-				//registrarUsuario(usuarios);
+				registrarUsuario(usuarios);
 				break;
 			case 2:
-				//registrarEvento(eventos);
+				registrarEvento(eventos);
 				break;
 			case 3:
 				//registrarOrganizadores(organizadores);
@@ -56,4 +56,50 @@ public class EventosSostenibles {
 			
 		}while(opcion!=9);
 	}
+public static void registrarEvento(HashMap<String, String[]> eventos) {
+	System.out.println("Nombre del evento: ");
+	String nombreEvento;
+	nombreEvento=sc.next();
+	System.out.println("Nombre del organizador:");
+	String organizador;
+	organizador=sc.next();
+	System.out.println("Fecha del evento: ");
+	String fechaEvento;
+	fechaEvento=sc.next();
+	System.out.println("Duración: ");
+	String duracion;
+	duracion=sc.next();
+	System.out.println("Ubicación: ");
+	String ubicacionEvento;
+	ubicacionEvento=sc.next();
+	System.out.println("Online o Presencial: ");
+	String tipo;
+	tipo=sc.next();
+	System.out.println("Aforo: ");
+	String aforo;
+	aforo=sc.next();
+	System.out.println("Categoria del evento: ");
+	String categoria;
+	categoria=sc.next();
+	System.out.println("Estado del evento: ");
+	String estado;
+	estado=sc.next();
+	String[] infoEvento= {organizador, fechaEvento, duracion, ubicacionEvento, tipo, aforo, categoria, estado};
+	eventos.put(nombreEvento, infoEvento);
+	
+}
+public static void registrarUsuario(HashMap<String, String[]> usuarios) {
+	System.out.println("Nombre: ");
+	String nombre;
+	nombre=sc.next();
+	System.out.println("Email: ");
+	String email;
+	email=sc.next();
+	System.out.println("Contraseña: ");
+	String contraseña;
+	contraseña=sc.next();
+	String[] datos= {nombre, contraseña};
+	usuarios.put(email, datos);
+	
+}
 }
